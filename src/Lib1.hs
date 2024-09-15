972d859d08738d7df1c18b28886a9924fbb772c9
Karolis Petrovskis <karolis.petrovskis@mif.stud.vu.lt>
2024-09-15 17:42:01 +0000

    Task 1

--- src/Lib1.hs
+++ src/Lib1.hs
@@ -1,8 +1,20 @@
 module Lib1
-    ( completions
-    ) where
+  ( completions,
+  )
+where
 
 -- | This function returns a list of words
 -- to be autocompleted in your program's repl.
 completions :: [String]
-completions = []
+completions =
+  [ "<facility>",
+    "<component>",
+    "<subsystem>",
+    "<component_list>",
+    "<energy_production_unit>",
+    "<solar_panel>",
+    "<nuclear_plant>",
+    "<hydro_plant>",
+    "<wind_turbine>",
+    "<max_storage_amount>"
+  ]
